d5d6c11e939df6f4d2ea59932218b4b70e66c28b
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-03-13 16:34:59 -0400

    Topology finished

--- TP1/topologia.py
+++ TP1/topologia.py
@@ -17,15 +17,16 @@ class Topologia(Topo):
         super(Topologia, self).__init__(**opts)
 
         # Add hosts and switches
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
-        h5 = self.addHost('h5')
-        h6 = self.addHost('h6')
-        h7 = self.addHost('h7')
-        h8 = self.addHost('h8')
-        h9 = self.addHost('h9')
+        h1 = self.addHost('h1', ip="10.0.0.1/24", defaultRoute = "via 10.0.0.254")
+
+        h2 = self.addHost('h2', ip="10.0.0.2/24", defaultRoute = "via 10.0.0.254")
+        h3 = self.addHost('h3', ip="10.0.0.3/24", defaultRoute = "via 10.0.0.254")
+        h4 = self.addHost('h4', ip="10.0.1.1/24", defaultRoute = "via 10.0.1.254")
+        h5 = self.addHost('h5', ip="10.0.1.2/24", defaultRoute = "via 10.0.1.254")
+        h6 = self.addHost('h6', ip="10.0.1.3/24", defaultRoute = "via 10.0.1.254")
+        h7 = self.addHost('h7', ip="10.0.2.1/24", defaultRoute = "via 10.0.2.254")
+        h8 = self.addHost('h8', ip="10.0.2.2/24", defaultRoute = "via 10.0.2.254")
+        h9 = self.addHost('h9', ip="10.0.2.3/24", defaultRoute = "via 10.0.2.254")
 
 
         # Adding switches
@@ -34,11 +35,14 @@ class Topologia(Topo):
         s3 = self.addSwitch('s3', dpid="0000000000000003")
         s4 = self.addSwitch('s4', dpid="0000000000000004")
 
+        #def 	addLink (self, node1, node2, port1=None, port2=None, cls=None, **params)
+       
         # Add links
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(h3, s1)
 
+       
         self.addLink(h4, s2)
         self.addLink(h5, s2)
         self.addLink(h6, s2, cls=TCLink, delay='5ms')
@@ -47,6 +51,6 @@ class Topologia(Topo):
         self.addLink(h8, s3)
         self.addLink(h9, s3, cls=TCLink, loss=10)
 
-        self.addLink(s1, s4, cls=TCLink, delay='5ms')
+        self.addLink(s1, s4, cls=TCLink, delay='5ms', params2={ 'ip' : '10.0.0.254/24' })
         self.addLink(s2, s4)
         self.addLink(s3, s4)
