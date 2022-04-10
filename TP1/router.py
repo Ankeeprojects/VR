00cbe6d2692bbdf5d606b59237a95848160a6248
AnkeeProjets <pmscpa@gmail.com>
2022-04-10 11:47:59 -0700

    Routes now distributed

--- TP1/router.py
+++ TP1/router.py
@@ -11,7 +11,12 @@ from ryu.lib.packet import ipv4
 from ryu.lib.packet import arp
 from ryu.lib.packet import ether_types
 from ryu.lib.packet import icmp
+from ryu.lib.packet import udp
+
 import ipaddress
+import threading
+import time
+import json
 
 
 #inicializar ryu em port definida - sudo ryu-manager --ofp-tcp-listen-port 6654 router.py
@@ -24,7 +29,8 @@ class Router(app_manager.RyuApp):
         
         self.mac_to_port = {}
         self.interfaces = dict()
-        
+        self.multicast_address = '224.0.0.10'
+
         self.interfaces[4] = {
             "10.0.1.254" : "ff:ff:ff:00:00:01", 
             "10.0.2.254" : "ff:ff:ff:00:00:02",
@@ -45,11 +51,11 @@ class Router(app_manager.RyuApp):
         }
 
         self.buffer = dict()
-        
+        """
         self.buffer[4] = dict()
         self.buffer[5] = dict()
         self.buffer[7] = dict()
-
+"""
 
         self.arp_helper = dict()
 
@@ -75,10 +81,68 @@ class Router(app_manager.RyuApp):
 
         self.arp_table = dict()
 
+        """
         self.arp_table[4] = dict()
         self.arp_table[5] = dict()
         self.arp_table[7] = dict()
+        """
+        self.routers = dict()
 
+        self.rotas = dict()
+        """
+        self.rotas[4] = dict()
+        self.rotas[5] = dict()
+        self.rotas[7] = dict()
+        """
+        
+        for id in [4,5,7]:
+            self.arp_table[id] = dict()
+            self.routers[id] = None
+            self.buffer[id] = dict()
+            self.rotas[id] = dict()
+            
+        threading.Thread(target=self.rip_announcements, args=(4,)).start()
+        threading.Thread(target=self.rip_announcements, args=(5,)).start()
+        threading.Thread(target=self.rip_announcements, args=(7,)).start()
+    
+    def rip_announcements(self, id):
+        while True:
+            time.sleep(3)
+            #self.logger.info(f"O meu DP é o {self.routers[id]}")
+
+            if self.rotas[id]:
+                self.send_rip_update(self.routers[id], self.rotas[id])
+
+    def send_rip_update(self, datapath, rotas):
+        for interface_ip, mac_add in self.interfaces[datapath.id].items():
+            port = self.get_port(datapath.id, interface_ip)
+            
+            e = ethernet.ethernet(src=mac_add, dst='ff:ff:ff:ff:ff:ff', ethertype=2048)
+            i = ipv4.ipv4(version=4, proto=17, src=interface_ip, dst=self.multicast_address)
+            u = udp.udp(src_port=36000, dst_port=36000)
+            
+            p = packet.Packet()
+            p.add_protocol(e)
+            p.add_protocol(i)
+            p.add_protocol(u)
+            p.add_protocol(json.dumps(rotas).encode('utf-8'))
+            p.serialize()
+
+            actions = [datapath.ofproto_parser.OFPActionOutput(port, 0)]
+
+            out = datapath.ofproto_parser.OFPPacketOut(
+                datapath=datapath,
+                buffer_id=0xffffffff,
+                in_port=datapath.ofproto.OFPP_CONTROLLER,
+                actions=actions,
+                data=p.data)
+        
+            datapath.send_msg(out)
+
+    def get_port(self, id, ip):
+        for entry in self.arp_helper[id]:
+            if entry[1] == ip:
+                return entry[2]
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -86,6 +150,7 @@ class Router(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
+        self.routers[datapath.id] = datapath
         # install the table-miss flow entry.
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
@@ -124,6 +189,11 @@ class Router(app_manager.RyuApp):
                 datapath.ofproto_parser.OFPActionSetField(eth_src=arp_packet.dst_mac),
                 datapath.ofproto_parser.OFPActionSetField(eth_dst=arp_packet.src_mac),
                 datapath.ofproto_parser.OFPActionOutput(in_port, 0)]
+
+        #(custo, prox hop, interface)
+        self.rotas[datapath.id][arp_packet.src_ip] = (1, arp_packet.src_ip, in_port)
+        
+        self.logger.info(f"TABELA DE ENCAMINHAMENTO DO {datapath.id}: {self.rotas[datapath.id]}")
 
         self.add_flow(datapath, 32768, match, actions)
 
@@ -205,8 +275,22 @@ class Router(app_manager.RyuApp):
                 self.logger.info(f"O pacote é {network}")
 
                 self.process_icmp(datapath, pkt, network, eth, msg.match['in_port'])
+        elif network and network.dst==self.multicast_address:
+            rotas = json.loads(pkt[-1])
+            
+            self.logger.info(f"SOU O {datapath.id} E RECEBI ROTAS DO {network.src}: {rotas}")
+            
+            self.add_rotas(rotas, datapath.id, network.src, msg.match['in_port'])
         #self.logger.info(network)
 
+    def add_rotas(self, rotas : dict, id : int, source : str, port : int):
+        for ip, dados in rotas.items():
+            comp = self.rotas[id]
+            if (ip in comp and dados[0]+1 < comp[ip][0])
+                self.rotas[id][ip] = [dados[0]+1, source, port]    
+            elif ip not in comp:
+                self.remove_flow()
+                self.rotas[id][ip] = [dados[0]+1, source, port]
     def reply_icmp(self, datapath, icmp_pkt, network, eth, port, packet):
         """
         if network.src not in self.arp_table:
@@ -304,6 +388,15 @@ class Router(app_manager.RyuApp):
         datapath.send_msg(out)
  
  #self.add_flow(datapath, arp_packet.src_ip, arp_packet.dst_mac, arp_packet.src_mac, in_port)
+    def remove_table_flows(self, datapath, table_id, match, instructions):
+        """Create OFP flow mod message to remove flows from table."""
+        ofproto = datapath.ofproto
+        flow_mod = datapath.ofproto_parser.OFPFlowMod(datapath, 0, 0,table_id, ofproto.OFPFC_DELETE, 0, 0, 1,ofproto.OFPCML_NO_BUFFER,
+                                                      ofproto.OFPP_ANY,
+                                                      OFPG_ANY, 0,
+                                                      match, instructions)
+        return flow_mod
+    
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
